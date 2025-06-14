406846004620ce31e90c314c022a48ea478d933c
SyriousXReaper <152575025+SyriousXReaper@users.noreply.github.com>
2025-06-13 20:31:59 -0400

    Create atividade27.py

---
+++ atividade27.py
@@ -0,0 +1,7 @@
+p1=float(input("Digite a primeira nota: "))
+p2=float(input("digite segunda nota: "))
+media= (p1+p2)/2
+if p1>p2:
+    print(f'sua maior nota foi {p1}, e a média das notas é {media}')
+else:
+    print(f'sua maior nota foi {p2}, e a média das notas é {media}')
