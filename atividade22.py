b0d635380676281b270ae2d110f16e3bebb82698
SyriousXReaper <152575025+SyriousXReaper@users.noreply.github.com>
2025-06-13 20:11:18 -0400

    Create atividade22.py

---
+++ atividade22.py
@@ -0,0 +1,15 @@
+verificacao = 0
+senha = 'admin'
+usuario = 'admin'
+for tentativa in range(3):
+    usuario = str(input('Digite o usuário: '))
+    senha = str(input('Digite a senha: '))
+    if usuario == 'admin' and senha == 'admin':
+        verificacao = 1
+        break
+    else:
+        print('Usuário ou senha errados, tente novamente')
+if verificacao == 1:
+    print('Acesso autorizado!')
+else:
+    print('Acesso negado!')
